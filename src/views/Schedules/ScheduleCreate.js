30f68381cd50f4cbc8c6c7fa9defb7fa3b64c036
timeworn <nikolay.voronin93@gmail.com>
2021-02-05 16:19:32 +0300

    fix: font changing on schedule create page

--- src/views/Schedules/ScheduleCreate.js
+++ src/views/Schedules/ScheduleCreate.js
@@ -17,6 +17,7 @@ import { SchedulesService } from 'core/services/schedules.service';
 const useStyle = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(4),
+    fontFamily: 'Roboto',
     [theme.breakpoints.down('sm')]: {
       padding: theme.spacing(2)
     },
